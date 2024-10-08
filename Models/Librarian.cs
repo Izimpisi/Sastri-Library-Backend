274c9eb61fbfa8b2596767a173bf236278e63530
Thobelani0107 <ziyanda1507@gmail.com>
2024-10-08 19:04:16 +0200

    adding models

---
+++ Models/Librarian.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Sastri_Library_Backend.Models
+{
+    public class Librarian
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Librarian_Id { get; set; }
+
+        [Required]
+         public string Lib_firstNam { get; set; }
+
+        [Required]
+        public string Lib_LastNam { get; set; }
+
+        [Required]
+        public string Lib_EmailAddr { get; set; }
+
+        [Required]
+        public string Lib_PhoneNum { get;set; }
+    }
+}
--- Models/Student.cs
+++ Models/Student.cs
@@ -1,6 +1,21 @@
-﻿namespace Sastri_Library_Backend.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Sastri_Library_Backend.Models
 {
     public class Student
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Student_ID { set; get; }
+
+        [Required]
+
+        public string first_Name { set; get; }
+
+        [Required]
+
+        public string last_Name { set; get; }
+
+     
     }
 }
